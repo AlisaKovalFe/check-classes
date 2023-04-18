5c0adf5167f7f89a1815bb83f50bbc5116d9daa5
Alisa <alisakovalhome@mail.ru>
2023-04-18 16:22:53 +0300

    Add solutions

--- carrier-rocket.js
+++ carrier-rocket.js
@@ -1,9 +1,11 @@
 class CarrierRocket {
   constructor(stages) {
-    
+    this.stages = stages.map((el) => el)
   }
 
-
+  detachStage() {
+    return this.stages.splice(0, 1)
+  }
 }
 
 module.exports = CarrierRocket;
